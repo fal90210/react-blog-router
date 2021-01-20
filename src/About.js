dbceb132f1281af919987d7e2612fed940a20cf3
Fal Pan <58827406+fal90210@users.noreply.github.com>
2021-01-20 15:49:16 -0500

    Initial commit

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+/**
+ * Simple about page (no navigation inside)
+ */
+
+export default function About() {
+  return (
+    <div className="about">
+      <h1>I am a mysterious person!</h1>
+      <p>I'm only available in the internet</p>
+    </div>
+  );
+}
---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+/**
+ * Simple contact page (no navigation inside)
+ */
+
+export default function Contact() {
+  return (
+    <div className="contact">
+      <p>
+        You can contact me via Ghost mail. Don't know what it is? Then you are
+        not a Ghost like me.
+      </p>
+    </div>
+  );
+}
